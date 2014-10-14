270d5cf62eaf8cbf9455d20e9afd29b484e89910
Brian Lauber <constructible.truth@gmail.com>
2014-10-13 22:48:11 -0400

    Prototyped solution for scenario 3

--- practice.rb
+++ practice.rb
@@ -1,35 +1,42 @@
 # This is where I'm trying new Ruby things
 
+4.times do
+  puts("UP")
+end
 
-# if 1 == 3
-#   puts("1 is equal to 3")
+puts("READ(MAP)")
+todo = 2
+
+if todo == 1
+  direction = "LEFT"
+else
+  direction = "RIGHT"
+end
+
+2.times do
+  puts(direction)
+end
+
+
+# if todo == 1
+#   2.times do
+#     puts("LEFT")
+#   end
 # else
-#   if 1 == 2
-#     puts("1 is equal to 2")
-#   else
-#     if 1 == 4
-#       puts("blah")
-#     else
-#       puts("1 is a special snowflake")
+#   2.times do
+#     puts("RIGHT")
 #   end
 # end
 
+# 2.times do
+#   if todo == 1
+#     puts("LEFT")
+#   else
+#     puts("RIGHT")
+#   end
+# end
 
-
-
-
-x = "hello"
-
-if !(x == 1)
-  puts("x is not equal to 1")
-elsif x == 2
-  puts("x is equal to 2")
-elsif x == 3
-  puts("x is equal to 3")
-else
-  puts("x is a special snowflake")
-end
-
+puts("DEFUSE(BOMB)")
 
 
 
