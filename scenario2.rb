845b36ba80b674b3f1f356fe89d49e408c7f70c0
Brian Lauber <constructible.truth@gmail.com>
2014-10-13 20:49:42 -0400

    Implemented scenario 2

---
+++ scenario2.rb
@@ -0,0 +1,24 @@
+# This is where I'm trying new Ruby things
+
+9.times do
+  puts("RIGHT")
+end
+
+2.times do
+  puts("DOWN")
+end
+
+9.times do
+  puts("LEFT")
+end
+
+2.times do
+  puts("DOWN")
+end
+
+9.times do
+  puts("RIGHT")
+end
+
+puts("EAT(BOMB)")
+
