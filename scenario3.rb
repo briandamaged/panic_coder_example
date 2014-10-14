c3bfa49ed0bccf7e6d383fe79f6d4e460dd45a0c
Brian Lauber <constructible.truth@gmail.com>
2014-10-13 22:48:35 -0400

    Moved code into scenario3.rb

---
+++ scenario3.rb
@@ -0,0 +1,42 @@
+# This is where I'm trying new Ruby things
+
+4.times do
+  puts("UP")
+end
+
+puts("READ(MAP)")
+todo = 2
+
+if todo == 1
+  direction = "LEFT"
+else
+  direction = "RIGHT"
+end
+
+2.times do
+  puts(direction)
+end
+
+
+# if todo == 1
+#   2.times do
+#     puts("LEFT")
+#   end
+# else
+#   2.times do
+#     puts("RIGHT")
+#   end
+# end
+
+# 2.times do
+#   if todo == 1
+#     puts("LEFT")
+#   else
+#     puts("RIGHT")
+#   end
+# end
+
+puts("DEFUSE(BOMB)")
+
+
+
