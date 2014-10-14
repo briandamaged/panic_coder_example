d0713909a2bf22ee1d41d15dd73174ceba91eaef
Brian Lauber <constructible.truth@gmail.com>
2014-10-13 21:19:07 -0400

    Demonstrated conditionals

--- practice.rb
+++ practice.rb
@@ -1,13 +1,35 @@
 # This is where I'm trying new Ruby things
 
-puts("RIGHT")
-puts("RIGHT")
-puts("RIGHT")
-puts("RIGHT")
-puts("DOWN")
-puts("DOWN")
-puts("DOWN")
-puts("DOWN")
-puts("EAT(BOMB)")
 
-# Extra comment
+# if 1 == 3
+#   puts("1 is equal to 3")
+# else
+#   if 1 == 2
+#     puts("1 is equal to 2")
+#   else
+#     if 1 == 4
+#       puts("blah")
+#     else
+#       puts("1 is a special snowflake")
+#   end
+# end
+
+
+
+
+
+x = "hello"
+
+if !(x == 1)
+  puts("x is not equal to 1")
+elsif x == 2
+  puts("x is equal to 2")
+elsif x == 3
+  puts("x is equal to 3")
+else
+  puts("x is a special snowflake")
+end
+
+
+
+
